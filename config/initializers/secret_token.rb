ee9dda2f78cddaa435a3d7ea2263a51a37e7fd79
Andrej Tazky <ayo@ynet.sk>
2011-10-30 09:33:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skusobna::Application.config.secret_token = 'e6320c0b9a2ccb72577111240273273162121b1ae29ef3b45066153c7e23578712ade8bb964821b4535139042706ec092a7b324e8f164dfb4497c0cdf3fbc113'
